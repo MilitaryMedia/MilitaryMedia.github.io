41fb3e50669b8ec395956811ddcc0ee996f520a2
mmurray1019 <157225528+mmurray1019@users.noreply.github.com>
2024-03-11 10:56:18 -0400

    Create konami.js

---
+++ konami.js
@@ -0,0 +1,14 @@
+function onKonamiCode(cb) {
+  var input = '';
+  var key = '38384040373937396665';
+  document.addEventListener('keydown', function (e) {
+    input += ("" + e.keyCode);
+    if (input === key) {
+      return cb();
+    }
+    if (!key.indexOf(input)) return;
+    input = ("" + e.keyCode);
+  });
+}
+
+onKonamiCode(function () {window.location.href = "https://militarymedia.github.io/Hampter.html";})
