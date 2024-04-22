1375eb9cac5b59bf467ed9596ac42fca3d94e34f
MilitaryMedia <158073687+MilitaryMedia@users.noreply.github.com>
2024-04-22 14:27:58 -0400

    Create secret.js

---
+++ secret.js
@@ -0,0 +1,14 @@
+function onSecretCode(cb) {
+  var input = '';
+  var key = '84189515213';
+  document.addEventListener('keydown', function (e) {
+    input += ("" + e.keyCode);
+    if (input === key) {
+      return cb();
+    }
+    if (!key.indexOf(input)) return;
+    input = ("" + e.keyCode);
+  });
+}
+
+onSecretCode(function () {window.location.href = "https://militarymedia.github.io/Hamster2.jpg";})
