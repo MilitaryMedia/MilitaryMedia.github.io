76902228fe47b88e41edadcd8b0a9ac9dbe1e237
mmurray1019 <157225528+mmurray1019@users.noreply.github.com>
2024-05-09 19:59:47 -0400

    Create game_code.js

---
+++ game_code.js
@@ -0,0 +1,14 @@
+function onKonamiCode(cb) {
+  var input = '';
+  var key = '716577698313';
+  document.addEventListener('keydown', function (e) {
+    input += ("" + e.keyCode);
+    if (input === key) {
+      return cb();
+    }
+    if (!key.indexOf(input)) return;
+    input = ("" + e.keyCode);
+  });
+}
+
+onKonamiCode(function () {window.location.href = "https://militarymedia.github.io/games.html";})
