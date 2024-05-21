62efa33b033fad36022aa7bfe0a1c233ed12ddfb
MilitaryMedia <158073687+MilitaryMedia@users.noreply.github.com>
2024-05-21 07:54:34 -0400

    Update and rename index.js to buttonChanger.js

---
+++ buttonChanger.js
@@ -0,0 +1,30 @@
+function textChanger(input)
+{
+    document.getElementById(input).textContent = 'Go to '+input;
+}
+    
+function textReverter(input)
+{
+    document.getElementById(input).textContent = input;
+}
+/*
+
+ADD THIS TO BUTTONS TO MAKE THEM INTERACTIVE!
+Change the Insert part to whatever the button is saposed to say.
+
+id="Insert" onmouseover="textChanger('Insert')" onmouseleave="textReverter('Insert')"
+
+*/
+
+function secretButton()
+{
+    document.getElementById(secretB).style.display = 'block';
+}
+function unSecretButton()
+{
+    document.getElementById(secretB).style.display = 'none';
+}
+function secretMessageButton1()
+{
+    document.getElementById(secretMessage1).style.display = 'block';
+}
