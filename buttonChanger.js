7f848d872a87733899beea2cb15f44236bc235db
MilitaryMedia <158073687+MilitaryMedia@users.noreply.github.com>
2024-05-22 10:53:12 -0400

    Update buttonChanger.js

--- buttonChanger.js
+++ buttonChanger.js
@@ -15,8 +15,31 @@ Change the Insert part to whatever the button is saposed to say.
 id="Insert" onmouseover="textChanger('Insert')" onmouseleave="textReverter('Insert')"
 
 */
+var logoclicktimes = 0;
+var message0 = 0;
+var message1 = 0;
+var click = 0;
 
 function secretMessageButton1()
+
 {
-    document.getElementById('secretMessage1').style.display = 'block';
+    click = click + 1;
+    document.getElementById("clicks").innerHTML = click;
+
+    logoclicktimes = logoclicktimes + 1;
+
+    if( message0 == 0 && logoclicktimes >= 5)
+    {
+        document.getElementById('secretButton1').style.display = 'block';
+        message0++;
+    }
+    if( message1 == 0 && logoclicktimes >= 1)
+    {
+        document.getElementById('clicks').style.display = 'block';
+        message1++;
+    }
+    if(logoclicktimes >= 1)
+    {
+        document.getElementById('clicks').innerHTML = 'you are at ' + click +' clicks.'
+    }
 }
