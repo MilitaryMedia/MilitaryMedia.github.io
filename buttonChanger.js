d7bb9d0587a70b18adbe0b8ee6fa70ed13d4f09f
Matthew Murray <157225528+mmurray1019@users.noreply.github.com>
2024-05-23 11:51:55 -0400

    clickers

--- buttonChanger.js
+++ buttonChanger.js
@@ -15,31 +15,53 @@ Change the Insert part to whatever the button is saposed to say.
 id="Insert" onmouseover="textChanger('Insert')" onmouseleave="textReverter('Insert')"
 
 */
-var logoclicktimes = 0;
+var click = 0;
 var message0 = 0;
 var message1 = 0;
-var click = 0;
 
-function secretMessageButton1()
+function prettify(input){
+    var output = Math.round(input * 1000000)/1000000;
+	return output;
+}
+
+function secretMessageButton1(input)
 
 {
-    click = click + 1;
-    document.getElementById("clicks").innerHTML = click;
+    click = click + input;
+    document.getElementById("clicks").innerHTML = prettify(click);
 
-    logoclicktimes = logoclicktimes + 1;
 
-    if( message0 == 0 && logoclicktimes >= 5)
+    if( message0 == 0 && click >= 5)
     {
         document.getElementById('secretButton1').style.display = 'block';
         message0++;
+        document.getElementById('cursorBuy').style.display = 'block';
     }
-    if( message1 == 0 && logoclicktimes >= 1)
+    if( message1 == 0 && click >= 1)
     {
         document.getElementById('clicks').style.display = 'block';
         message1++;
     }
-    if(logoclicktimes >= 1)
+    if(click >= 1)
     {
         document.getElementById('clicks').innerHTML = 'you are at ' + click +' clicks.'
     }
 }
+var cursors = 0;
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    if(click >= cursorCost){                                   //checks that the player can afford the cursor
+        cursors = cursors + 1;                                   //increases number of cursors
+    	click = click - cursorCost;                          //removes the cookies spent
+        document.getElementById('cursors').innerHTML = prettify(cursors);  //updates the number of cursors for the user
+        document.getElementById('cookies').innerHTML = prettify(click);  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    document.getElementById('cursorCost').innerHTML = prettify(nextCost);  //updates the cursor cost for the user
+};
+
+window.setInterval(function(){
+	
+	secretMessageButton1(cursors);
+	
+}, 1000);
